cd38470cab881a9d52e53c608ae2c31e2c955259
mccalldan <drm8266@gmail.com>
2017-07-26 22:48:41 -0400

    almost there

--- liri.js
+++ liri.js
@@ -50,24 +50,61 @@ function myTweets() {
 
 // Spotifying Songs
 
-function spotifyThisSong() {
+function spotifyThisSong(keyword) {
 
+var keyword = process.argv[3];
+var spotify = new Spotify({
+  id: 'e86b36c6edf64299a90090662e191562',
+  secret: '44c7505bd44f4f5eb222e5b2fda65131'
+  });
 
+    spotify.search({ type: 'track', query: keyword || 'The Sign Ace of Base' }, function(err, data) {
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+      }
+
+     if(data.tracks.items.length > 0) {
+        var record = data.tracks.items[0];
+
+        console.log(' ');
+        console.log('================================');
+        console.log('Artist: ' + record.artists[0].name);
+        console.log('Name: ' + record.name);
+        console.log('Link: ' + record.preview_url);
+        console.log('Album: ' + record.album.name);
+        console.log(' ');
+       } 
+
+      else {
+        console.log('No song data found.');
+      }
+   });
 }
 
 // Movie Info
 
-function movieThis() {
+function movieThis(query) {
 
-/*inquirer.prompt([
-    {
-      type: "input",
-      message: "What movie are you interested in?",
-      name: "movie",
-      
-    }
-    ])*/
+    var query = process.argv[3];
 
-    var movieName = process.argv[3];
-    console.log(movieName);
- }
+    request('http://www.omdbapi.com/?t=' + (query || 'Mr.Nobody') +'&tomatoes=true&apikey=40e9cece', function (error, response, info) {
+      if (!error && response.statusCode == 200) {
+
+       var movieData = JSON.parse(info);
+
+       console.log(' ');
+        console.log('================================');
+        console.log('Title: ' + movieData.Title);
+        console.log('Year: ' + movieData.Year);
+        console.log('IMDB Rating: ' + movieData.imdbRating);
+        console.log('Rotten Tomatoes Rating: ' + movieData.tomatoRating);
+        console.log('Country: ' + movieData.Country);
+        console.log('Language: ' + movieData.Language);
+        console.log('Plot: ' + movieData.Plot);
+        console.log('Actors: ' + movieData.Actors);
+        console.log(' ');     
+      }
+    });
+  };
+  
--- liri2.js
+++ liri2.js
@@ -20,36 +20,97 @@ switch (action) {
     case "movie-this":
     movieThis();
     break;
+
+    case "do-what-it-says":
+    doWhatItSays();
+    break;
 }
 
 
 
+function spotifyThisSong(keyword) {
+
+    var keyword = process.argv[3];
+    var spotify = new Spotify({
+    id: 'e86b36c6edf64299a90090662e191562',
+    secret: '44c7505bd44f4f5eb222e5b2fda65131'
+    });
+
+    spotify.search({ type: 'track', query: keyword || 'The Sign Ace of Base' }, function(err, data) {
+      if ( err ) {
+          console.log('Error occurred: ' + err);
+          return;
+      }
+
+     if(data.tracks.items.length > 0) {
+        var record = data.tracks.items[0];
+
+        console.log(' ');
+        console.log('================================');
+        console.log('Artist: ' + record.artists[0].name);
+        console.log('Name: ' + record.name);
+        console.log('Link: ' + record.preview_url);
+        console.log('Album: ' + record.album.name);
+        console.log(' ');
+      } 
+
+      else {
+        console.log('No song data found.');
+      }
+   });
+}
 
 
+ function movieThis(query) {
 
+    var query = process.argv[3];
 
-function myTweets() {
-	var client = new Twitter(keys.twitterKeys);
-	var params = {screen_name: 'nocomment919'};
-	client.get('statuses/user_timeline',params, function(error, tweets, response) {
-  	if(!error) /*throw error*/ {}
-  		//console.log(tweets);  // The favorites. 
-  		//console.log(response);  // Raw response object.
-        /*for(var i=0; i < 20; i++) {
-        if(tweets[i]) {
-          console.log(tweets[i].text);*/
-      console.log("--------My Tweets--------");
-        tweets.forEach(function(obj) {
-          for(var i = 0; i < 20; i++) {
-        console.log("Time: " + obj.created_at);
-        console.log("Tweet: " + obj.text);}
-        });
+    request('http://www.omdbapi.com/?t=' + (query || 'Mr.Nobody') +'&tomatoes=true&apikey=40e9cece', function (error, response, info) {
+      if (!error && response.statusCode == 200) {
+
+       var movieData = JSON.parse(info);
+
+       console.log(' ');
+        console.log('================================');
+        console.log('Title: ' + movieData.Title);
+        console.log('Year: ' + movieData.Year);
+        console.log('IMDB Rating: ' + movieData.imdbRating);
+        console.log('Rotten Tomatoes Rating: ' + movieData.tomatoRating);
+        console.log('Country: ' + movieData.Country);
+        console.log('Language: ' + movieData.Language);
+        console.log('Plot: ' + movieData.Plot);
+        console.log('Actors: ' + movieData.Actors);
+        console.log(' ');
+
       
-      /*else { console.log(error);}*/
+      }
+    });
+  };
+
+
+function doWhatItSays(){
+
+  command1 = process.argv[2];
+  command2 = process.argv[3];
+
+  fs.readFile('random.txt', 'utf8', command1, command2, function(err, data) {
+      if(err) throw err;
+      console.log(data.toString());
+
+     var rand = data.toString().split(',');
+     
+     
+
     
-  
+    });
+  }
 
-});
+function logData(data) {
+    fs.appendFile('log.txt', JSON.stringify(data, null, 2) + '\n====================================================================================', function(err) {
+      if(err) {
+        console.log(err);
+      }
+    });
+  }
 
-}
 
